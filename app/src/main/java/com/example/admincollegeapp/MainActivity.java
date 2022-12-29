5cf232f4b490e0c331c80b28823e30d9799ab931
Anujkes <anujkes96@gmail.com>
2022-12-29 23:06:30 +0530

    before upload image activity

--- app/src/main/java/com/example/admincollegeapp/MainActivity.java
+++ app/src/main/java/com/example/admincollegeapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.admincollegeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView uploadNotice,uploadImage,uploadEbook,updateFaculty,deleteNotice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        uploadNotice=findViewById(R.id.uploadNotice);
+        uploadImage=findViewById(R.id.uploadImage);
+        uploadEbook=findViewById(R.id.uploadEbook);
+        updateFaculty=findViewById(R.id.updateFaculty);
+        deleteNotice=findViewById(R.id.deleteNotice);
+
+
+
     }
 }
