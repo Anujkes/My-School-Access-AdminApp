b4593c607d4690b2d4635202bfd6902d1d28aa34
Anujkes <anujkes96@gmail.com>
2022-12-30 00:42:28 +0530

    upload activity

--- app/src/main/java/com/example/admincollegeapp/MainActivity.java
+++ app/src/main/java/com/example/admincollegeapp/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.admincollegeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView uploadNotice,uploadImage,uploadEbook,updateFaculty,deleteNotice;
+        CardView uploadNotice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
 
         uploadNotice=findViewById(R.id.uploadNotice);
-        uploadImage=findViewById(R.id.uploadImage);
-        uploadEbook=findViewById(R.id.uploadEbook);
-        updateFaculty=findViewById(R.id.updateFaculty);
-        deleteNotice=findViewById(R.id.deleteNotice);
+       // uploadImage=findViewById(R.id.uploadImage);
+      //  uploadEbook=findViewById(R.id.uploadEbook);
+      //  updateFaculty=findViewById(R.id.updateFaculty);
+       // deleteNotice=findViewById(R.id.deleteNotice);
+
+
+        uploadNotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(MainActivity.this,UploadNotice.class));
+            }
+        });
 
 
 
