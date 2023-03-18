ea59076a6af289579b1fed99d3c414979c912945
Anujkes <anujkes96@gmail.com>
2023-03-18 20:09:42 +0530

    Admin Login activity added

--- app/src/main/java/com/example/admincollegeapp/MainActivity.java
+++ app/src/main/java/com/example/admincollegeapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,7 +14,9 @@ import com.example.admincollegeapp.notice.UploadNoticeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CardView uploadNotice,uploadImage,uploadEbook,updateFaculty,deleteNotice;
+    private CardView uploadNotice,uploadImage,uploadEbook,updateFaculty,deleteNotice,logout;
+    private SharedPreferences sharedPreference;
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
         uploadEbook=findViewById(R.id.uploadEbook);
         updateFaculty=findViewById(R.id.updateFaculty);
         deleteNotice=findViewById(R.id.deleteNotice);
+        logout=findViewById(R.id.logout);
+
+
+        sharedPreference=this.getSharedPreferences("login",MODE_PRIVATE);
+        editor = sharedPreference.edit();
+
+
 
 
         uploadNotice.setOnClickListener(new View.OnClickListener() {
@@ -65,6 +75,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, DeleteNoticeActivity.class));
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                editor.putString("isLogin","false");
+                editor.commit();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
             }
         });
 
